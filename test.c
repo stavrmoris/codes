3660854e2e9c8a6bc74f6712a8f9d34a52ee55bc
Stavr Moris <110993331+stavrmoris@users.noreply.github.com>
2026-04-29 18:20:36 +0700

    Update test.c

--- test.c
+++ test.c
@@ -1,28 +1,18 @@
-Вот ключевая штука: ты грузился через Advanced в GRUB, значит все наши правки в extlinux.conf вообще не участвовали. Мы правили не тот загрузочный путь.
+Делаем так
+На рабочей SD-системе под 6.12:
 
-Надо править GRUB cmdline для ядра 6.6.89-cix.
+cp -av /boot/initrd-6.6.89-cix.img /boot/initrd-6.6.89-cix.img.sd-bak 2>/dev/null || true
+cp -av /mnt/ssd/boot/initrd-6.6.89-cix.img /boot/initrd-6.6.89-cix.img
+cp -av /mnt/ssd/lib/firmware/* /lib/firmware/ 2>/dev/null || true
+sync
+Потом проверь, что файл реально заменился
+ls -l /boot/initrd-6.6.89-cix.img
+ls -l /mnt/ssd/boot/initrd-6.6.89-cix.img
+Размеры должны быть одинаковые или очень близкие.
 
-Сделай так на рабочем 6.12:
-
-cp -a /etc/default/grub /etc/default/grub.bak
-grep -n 'GRUB_CMDLINE' /etc/default/grub
-nano /etc/default/grub
-Найди/поставь такие строки:
-
-GRUB_CMDLINE_LINUX="panic=30 nomodeset cma=1300M iommu=off noresume"
-GRUB_CMDLINE_LINUX_DEFAULT=""
-Сохрани, потом:
-
+Дальше не трогаем параметры, просто обновим grub ещё раз
 grub-mkconfig -o /boot/grub/grub.cfg
-Проверь, что параметры реально попали в 6.6.89-cix:
-
 grep -nA2 -B1 "vmlinuz-6.6.89-cix" /boot/grub/grub.cfg
-Там должна быть строка с чем-то вроде:
-
-linux /boot/vmlinuz-6.6.89-cix root=UUID=... panic=30 nomodeset cma=1300M iommu=off noresume
-Потом:
-
+И снова пробуем
 reboot
-И снова выбирай в Advanced ядро 6.6.89-cix.
-
-Если опять будет black screen, следующий шаг уже не в параметрах, а в копировании точного рабочего initrd/EFI-запуска со старого SSD. Но сейчас сначала добьём именно GRUB, потому что до этого мы били мимо цели.
+Выбирай тот же 6.6.89-cix в Advanced.
